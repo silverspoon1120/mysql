2b8a073c99964cee45d86cc466fa4929df3b0518
Silver Spoon <silverspoon1120.sw@gmail.com>
2018-06-26 15:06:33 +0200

    Ignore boolean keys in my.cnf. (#283)

--- mysqld_exporter.go
+++ mysqld_exporter.go
@@ -60,11 +60,16 @@ var scrapers = map[collector.Scraper]bool{
 	collector.ScrapeEngineTokudbStatus{}:              false,
 	collector.ScrapeEngineInnodbStatus{}:              false,
 	collector.ScrapeHeartbeat{}:                       false,
+	collector.ScrapeSlaveHosts{}:                      false,
 }
 
 func parseMycnf(config interface{}) (string, error) {
 	var dsn string
-	cfg, err := ini.Load(config)
+	opts := ini.LoadOptions{
+		// MySQL ini file can have boolean keys.
+		AllowBooleanKeys: true,
+	}
+	cfg, err := ini.LoadSources(opts, config)
 	if err != nil {
 		return dsn, fmt.Errorf("failed reading ini file: %s", err)
 	}
